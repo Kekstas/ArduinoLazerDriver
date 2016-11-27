9a5ec78d0bd0cbcbb8707741ee4e05897a9a6fb0
Justas Sasnauskas <justas.sasnauskas@visma.com>
2016-11-27 23:21:41 +0200

    With Timer1 Only working

--- LazerBlockA.ino
+++ LazerBlockA.ino
@@ -18,29 +18,58 @@ Tranzistors ActiveTranzistor = unknown;
 
 void setup()
 {
-        
-    digitalWrite (PinLed, LOW);
-    
-    digitalWrite (PinA, LOW);
-    digitalWrite (PinB, LOW);
-  
-    
-  pinMode(PinLed, OUTPUT);
-  pinMode(PinA, OUTPUT);
-  pinMode(PinB, OUTPUT);
-  
-  // initialize timer1 
-  noInterrupts();           // disable all interrupts
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1  = 0;
+ 
+          noInterrupts();           // disable all interrupts
 
-  OCR1A = 31250;            // compare match register 16MHz/256/2Hz
-  TCCR1B |= (1 << WGM12);   // CTC mode
-  TCCR1B |= (1 << CS12);    // 256 prescaler 
-  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-  interrupts();             // enable all interrupts
+
+// use only one of the following 3 lines
+//GTCCR = (1<<TSM)|(1<<PSRASY)|(1<<PSRSYNC); // halt all timers
+//GTCCR = (1<<TSM)|(1<<PSRASY)|(0<<PSRSYNC); // halt timer2 only
+GTCCR = (1<<TSM)|(0<<PSRASY)|(1<<PSRSYNC); // halt timer0 and timer1 only
+
+// place all timer setup code here
+// do not do any timer setup before this section
+
+// set all timers to the same value Syncronyzing
+TCNT0 = 0; // set timer0 to 0
+TCNT1H = 0; // set timer1 high byte to 0
+TCNT1L = 0; // set timer1 low byte to 0
+
+
+//Setting Timers mode
+
+          pinMode(9, OUTPUT);
+          pinMode(10, OUTPUT);
+          TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(COM1B0)  ;  
+          TCCR1B=_BV(WGM13) |_BV(CS12);       //  PWM, Phase and Frequency Correct with TOP ICR1 +   clkI/O/256 (From prescaler)
+          
+          ICR1=10000 ;        // compare match register 16MHz/256/2Hz
+
+          OCR1A = 5000;
+          OCR1B = 5000;
+
+          
+
+GTCCR = 0; // release all timers
+
+
+
+ 
 }
+
+/*
+16.9.5 Phase and Frequency Correct PWM Mode
+
+In phase and frequency correct PWM mode the counter is incremented until the counter value matches either
+the value in ICR1 (WGM13:0 = 8), or the value in OCR1A (WGM13:0 = 9). The counter has then reached the
+TOP and changes the count direction. The TCNT1 value will be equal to TOP for one timer clock cycle. The
+timing diagram for the phase correct and frequency correct PWM mode is shown on Figure 16-9 on page 128.
+The figure shows phase and frequency correct PWM mode when OCR1A or ICR1 is used to define TOP. The
+TCNT1 value is in the timing diagram shown as a histogram for illustrating the dual-slope operation. The
+diagram includes non-inverted and inverted PWM outputs. The small horizontal line marks on the TCNT1 slopes
+represent compare matches between OCR1x and TCNT1. The OC1x Interrupt Flag will be set when a compare
+match occurs.
+
 
 ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
@@ -60,6 +89,7 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
   }  
   digitalWrite(PinLed, digitalRead(PinLed) ^ 1);   // toggle LED pin
 }
+*/
 
 void loop()
 {
