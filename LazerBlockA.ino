5220b8c22e6f26936ce1626148c1a95b6f987a68
Justas Sasnauskas <justas.sasnauskas@visma.com>
2016-11-28 21:40:07 +0200

    Periodo reguliavimo debuginimas

--- LazerBlockA.ino
+++ LazerBlockA.ino
@@ -1,82 +1,108 @@
-const byte adcPin = 0;
+
+// _______                         _________________                         _________________
+//        |_______________________|                 |_______________________|                 |_______
+//            _________________                         _________________                         ____
+// __________|                 |_______________________|                 |_______________________|            
+//        x--x  
+//           y-----------------------------------------y
+//
+
+
+#define  MaksimalusPeriodas    1600    // =16MH*100us  y--y
+#define  MinimalusPeriodas     1120    // =16MH*70us   y--y
+#define  PersijungimoPeriodas    80    // =16MH*5us    x--x
+#define  PeriodoPotenciometroPin  0
+
+#define  TranzistorAPin 9    // Nekeiciami
+#define  TranzistorBPin 10   // Nekeiciami
+
+
+
 volatile int adcReading;
 volatile boolean adcDone;
 boolean adcStarted;
 
+unsigned long DarbinisPeriodas;
+unsigned long TeigiamasOCR1B;
+unsigned long NeigiamasOCR1A;
+
+
+
+void RestartTranzistors(void)//unsigned long Period, unsigned long TeigiamasOCR1B, unsigned long NeigiamasOCR1A)
+{    
+      digitalWrite (TranzistorAPin, LOW);
+      digitalWrite (TranzistorBPin, LOW);
+      
+      pinMode(TranzistorAPin, OUTPUT);
+      pinMode(TranzistorBPin, OUTPUT);
+      
+      //halting timer0 and timer1 only
+      GTCCR = (1<<TSM)|(0<<PSRASY)|(1<<PSRSYNC); 
+
+      //Reseting Timer1
+      TCNT1H = 0; // set timer1 high byte to 0
+      TCNT1L = 0; // set timer1 low byte to 0
+      
+      //Setting Timers mode
+      TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(COM1B0)  ;  
+      //TCCR1B=_BV(WGM13) |_BV(CS10);       //  PWM, Phase and Frequency Correct with TOP ICR1 +   clkI/O/   (  NO prescaler)
+      TCCR1B=_BV(WGM13) |_BV(CS12)|_BV(CS10);       //  PWM, Phase and Frequency Correct with TOP ICR1 +   clkI/O/1024 (From prescaler)
+      
+      ICR1=DarbinisPeriodas ;        // compare match register 16MHz/256/2Hz
+      
+      OCR1A = NeigiamasOCR1A;
+      OCR1B = TeigiamasOCR1B;
+           
+      
+      GTCCR = 0; // release all timers
+  
+}
+
 void setup()
 {
  
-          //noInterrupts();           // disable all interrupts
+    //noInterrupts();           // disable all interrupts
+    //setting ADC
+    ADCSRA =  bit (ADEN);                      // turn ADC on
+    ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // Seting Prescaler for  ADC clock --- >128
+    ADMUX  =  bit (REFS0) | (PeriodoPotenciometroPin & 0x07);    // AVcc and select input port
+    //interrupts();
 
-
-// use only one of the following 3 lines
-//GTCCR = (1<<TSM)|(1<<PSRASY)|(1<<PSRSYNC); // halt all timers
-//GTCCR = (1<<TSM)|(1<<PSRASY)|(0<<PSRSYNC); // halt timer2 only
-GTCCR = (1<<TSM)|(0<<PSRASY)|(1<<PSRSYNC); // halt timer0 and timer1 only
-
-// place all timer setup code here
-// do not do any timer setup before this section
-
-// set all timers to the same value Syncronyzing
-TCNT0 = 0; // set timer0 to 0
-TCNT1H = 0; // set timer1 high byte to 0
-TCNT1L = 0; // set timer1 low byte to 0
-
-
-//Setting Timers mode
-
-          pinMode(9, OUTPUT);
-          pinMode(10, OUTPUT);
-          TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(COM1B0)  ;  
-          TCCR1B=_BV(WGM13) |_BV(CS12);       //  PWM, Phase and Frequency Correct with TOP ICR1 +   clkI/O/256 (From prescaler)
-          
-          ICR1=10000 ;        // compare match register 16MHz/256/2Hz
-
-          OCR1A = 5000;
-          OCR1B = 5000;
-
-          
-
-GTCCR = 0; // release all timers
-
-//setting ADC
-ADCSRA =  bit (ADEN);                      // turn ADC on
-ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // Seting Prescaler for  ADC clock --- >128
-ADMUX  =  bit (REFS0) | (adcPin & 0x07);    // AVcc and select input port
-
-
-  
-//Interrupts();
-
-
-//DEBUGING
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println ("Test setup"); 
+    //DEBUGING
+    Serial.begin(9600);
+    delay(1000);
+    Serial.println ("Test setup"); 
 }
+
 
 // ADC complete ISR
 ISR (ADC_vect)
-  {
+{
   adcReading = ADC;
   adcDone = true;  
-  }  // end of ADC_vect
-  
-  
+} 
 
 void loop()
 {
    
  // if last reading finished, process it
   if (adcDone)
-    {
-    adcStarted = false;
+    {      
+      Serial.print ("adcReading:");    
+      Serial.println (adcReading);    
+      
+      DarbinisPeriodas = ((MaksimalusPeriodas-MinimalusPeriodas)/1023*adcReading+MinimalusPeriodas)/2 ;
+      TeigiamasOCR1B=DarbinisPeriodas/2-PersijungimoPeriodas/2;
+      NeigiamasOCR1A=TeigiamasOCR1B+PersijungimoPeriodas;
 
-    // do something with the reading, for example, print it
-    Serial.println (adcReading);
-    delay (500);
+      Serial.print ("DarbinisPeriodas:");    
+      Serial.println (DarbinisPeriodas);   
 
-    adcDone = false;
+      RestartTranzistors();//DarbinisPeriodas, TeigiamasOCR1B, NeigiamasOCR1A);
+
+      adcStarted = false;
+      adcDone = false;
+      delay (5000);
     }
     
   // if we aren't taking a reading, start a new one
